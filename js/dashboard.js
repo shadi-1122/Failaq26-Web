068c50c8e8e07b69048bdbd634ad6ef1718a3a4f
shadi-1122 <shahinshadi1122@gmail.com>
2025-02-20 21:28:56 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -431,7 +431,7 @@ let studentsData = [];
             sh.classList.toggle('active')
         }
 
-        const GITHUB_TOKEN = "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"; // Replace with your token
+        const GITHUB_TOKEN = "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"; // Replace with your token
         const REPO_OWNER = "shadi-1122";
         const REPO_NAME = "Failaq26-Web";
         const FILE_PATH = "users.json";
